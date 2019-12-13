77acec7d81d38d087ab2b1b26122b0a337f5f445
tristansiquoir <archtemps@hotmail.fr>
2019-12-13 15:49:40 +0100

    add final modif

--- class/Shape.php
+++ class/Shape.php
@@ -12,15 +12,31 @@ abstract class Shape
 
     abstract public function perimeter();
 
-    public function displayPerimeter()
+    public function displayPerimeter($unit = 'cm')
     {
-        echo "Le " . $this->name . " a un périmétre de " . $this->perimeter() . self::UNIT;
+        if ($unit === 'mm') {
+            $perimeter = $this->perimeteToMm();
+        } else {
+            $perimeter = $this->perimeter();
+
+        }
+
+        echo "Le " . $this->name . " a un périmétre de " . $perimeter . $unit;
     }
 
     public function displayArea()
     {
         echo "Le " . $this->name . " a une aire de " . $this->area() . self::UNIT . '2';
     }
+
+    public function perimeteToMm()
+    {
+        return $this->perimeter() * 10;
+    }
     
+    public function areaToMm()
+    {
+        return $this->area() * 10;
+    }
 }
 
